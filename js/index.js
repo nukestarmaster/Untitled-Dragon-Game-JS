5c639ed850d7314636ec2643bde363d1ecdb335a
nukestarmaster <69655738+nukestarmaster@users.noreply.github.com>
2025-02-24 16:41:56 -0800

    Initial setup

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import { createApp } from "./vue";
+
+const app = createApp({
+    data: {
+        player
+    }
+}).mount("#app")
---
+++ js/player.js
@@ -0,0 +1,18 @@
+var player = {
+    vitals: [],
+    resources: [],
+    actions: [],
+    skills: [],
+    attributes: [],
+    spiritAttributes: [],
+    effects: [],
+    inventory: []
+}
+
+var health = {
+    name: "Health",
+    max: 5,
+    current: 5
+}
+
+player.vitals.push(health)
