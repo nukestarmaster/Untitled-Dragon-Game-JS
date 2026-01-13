a32b7f7140fb134f2a92964bddf0c28a30b5d170
nukestarmaster <nukestarmaster@gmail.com>
2026-01-12 16:58:05 -0800

    remove console logging

--- src/player.js
+++ src/player.js
@@ -87,9 +87,6 @@ const player = {
         }
         let spirits = this.spirits.data
         for (let s in spirits) {
-            console.log(spirits[s].id)
-            console.log(spirits[s].visible)
-            console.log(spirits[s].baseLevel)
             if(spirits[s].visible && spirits[s].baseLevel >= 1) {
                 this.attributes.data[spirits[s].id].show()
             }
